37fc1943684b3627b91b68334ac7539ee2db63a6
hais1992 <hais1992@163.com>
2017-08-01 13:03:23 +0800

    x

--- vendor.mk
+++ vendor.mk
@@ -1,10 +1,10 @@
 PRODUCT_COPY_FILES += \
     vendor/supersu/common/install-recovery.sh:system/etc/install-recovery.sh \
     vendor/supersu/common/install-recovery.sh:system/bin/install-recovery.sh \
-    vendor/supersu/armv7/su:system/xbin/su \
-    vendor/supersu/armv7/su:system/bin/.ext/.su \
-    vendor/supersu/armv7/su:system/xbin/daemonsu \
-    vendor/supersu/armv7/supolicy:system/xbin/supolicy \
-    vendor/supersu/armv7/libsupol.so:system/lib/libsupol.so
+    vendor/supersu/arm64/su:system/xbin/su \
+    vendor/supersu/arm64/su:system/bin/.ext/.su \
+    vendor/supersu/arm64/su:system/xbin/daemonsu \
+    vendor/supersu/arm64/supolicy:system/xbin/supolicy \
+    vendor/supersu/arm64/libsupol.so:system/lib/libsupol.so
 
 PRODUCT_PACKAGES += SuperSU
