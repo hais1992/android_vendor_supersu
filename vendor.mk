3f0bf4743786c95d7bbea5434b7e1dfd9ac632b4
hais1992 <hais1992@163.com>
2017-08-09 17:28:42 +0800

    胡乱修改~

--- vendor.mk
+++ vendor.mk
@@ -1,10 +1,10 @@
 PRODUCT_COPY_FILES += \
-    vendor/zuk/z2_plus/supersu/common/install-recovery.sh:system/etc/install-recovery.sh \
-    vendor/zuk/z2_plus/supersu/common/install-recovery.sh:system/bin/install-recovery.sh \
-    vendor/zuk/z2_plus/supersu/arm64/su:system/xbin/su \
-    vendor/zuk/z2_plus/supersu/arm64/su:system/bin/.ext/.su \
-    vendor/zuk/z2_plus/supersu/arm64/su:system/xbin/daemonsu \
-    vendor/zuk/z2_plus/supersu/arm64/supolicy:system/xbin/supolicy \
-    vendor/zuk/z2_plus/supersu/arm64/libsupol.so:system/lib/libsupol.so
+    vendor/zuk/z2_plus/SuperSU/common/install-recovery.sh:system/etc/install-recovery.sh \
+    vendor/zuk/z2_plus/SuperSU/common/install-recovery.sh:system/bin/install-recovery.sh \
+    vendor/zuk/z2_plus/SuperSU/arm64/su:system/xbin/su \
+    vendor/zuk/z2_plus/SuperSU/arm64/su:system/bin/.ext/.su \
+    vendor/zuk/z2_plus/SuperSU/arm64/su:system/xbin/daemonsu \
+    vendor/zuk/z2_plus/SuperSU/arm64/supolicy:system/xbin/supolicy \
+    vendor/zuk/z2_plus/SuperSU/arm64/libsupol.so:system/lib/libsupol.so
 
 PRODUCT_PACKAGES += SuperSU
