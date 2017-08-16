a08317ecacc1baad50d89e95dd8ae25555069d35
hais1992 <hais1992@163.com>
2017-08-16 13:24:18 +0800

    更新 root

--- vendor.mk
+++ vendor.mk
@@ -1,10 +1,11 @@
 PRODUCT_COPY_FILES += \
     vendor/zuk/z2_plus/SuperSU/common/install-recovery.sh:system/etc/install-recovery.sh \
     vendor/zuk/z2_plus/SuperSU/common/install-recovery.sh:system/bin/install-recovery.sh \
+    vendor/zuk/z2_plus/SuperSU/common/99SuperSUDaemon:system/etc/init.d/99SuperSUDaemon  \
     vendor/zuk/z2_plus/SuperSU/arm64/su:system/xbin/su \
     vendor/zuk/z2_plus/SuperSU/arm64/su:system/bin/.ext/.su \
     vendor/zuk/z2_plus/SuperSU/arm64/su:system/xbin/daemonsu \
     vendor/zuk/z2_plus/SuperSU/arm64/supolicy:system/xbin/supolicy \
-    vendor/zuk/z2_plus/SuperSU/arm64/libsupol.so:system/lib/libsupol.so
+    vendor/zuk/z2_plus/SuperSU/arm64/libsupol.so:system/lib/libsupol.so 
 
 PRODUCT_PACKAGES += SuperSU
