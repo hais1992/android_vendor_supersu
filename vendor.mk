4668e53fbc2f74b9c4e93496770c3d0b67c4e0f0
hais1992 <hais1992@163.com>
2017-08-24 10:16:43 +0800

    去除多余文件

--- vendor.mk
+++ vendor.mk
@@ -5,7 +5,6 @@ PRODUCT_COPY_FILES += \
     vendor/zuk/z2_plus/SuperSU/arm64/su:system/bin/.ext/.su \
     vendor/zuk/z2_plus/SuperSU/su:system/xbin/daemonsu \
     vendor/zuk/z2_plus/SuperSU/supolicy:system/xbin/supolicy \
-    vendor/zuk/z2_plus/SuperSU/armv7/libsupol.so:system/lib/libsupol.so \
     vendor/zuk/z2_plus/SuperSU/arm64/libsupol.so:system/lib64/libsupol.so
 
 PRODUCT_PACKAGES += SuperSU
